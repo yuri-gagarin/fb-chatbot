1a8d83d3868331ca5ec3af0b6005998b930021f8
Yuriy Ivanov <ymivanov@icloud.com>
2019-02-04 17:16:30 +0200

    webhook modification

--- routes/verifyWebhook.js
+++ routes/verifyWebhook.js
@@ -2,14 +2,17 @@ const verifyWebhook = (request, response) => {
   let VERIFY_TOKEN = 'yuriy-chatbot';
 
   let mode = request.query['hub.mode'];
-  let token = request.query['hub.verify-token'];
+  let token = request.query['hub.verify_token'];
   let challenge = request.query['hub.challenge'];
 
-  if (mode && token === VERIFY_TOKEN) {
-    response.status(200);
-    response.send(challenge);
-  } else {
-    response.status(403);
+  if (mode && token) {
+    if (token === VERIFY_TOKEN) {
+      console.log('webhook verified');
+      response.status(200);
+      response.send(challenge);
+    } else {
+      response.status(403);
+    }
   }
 };
 
